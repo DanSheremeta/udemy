9a2fc6ca842968224967f8f8424d80e3b54061a2
qioto <danylo3002@gmail.com>
2022-09-01 01:56:50 +0300

    Chapter 5: Views and Serializers     19. Model Serializer

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -3,26 +3,13 @@ from rest_framework import serializers
 from ..models import Movie
 
 
-def name_len(value):
-    if len(value) < 2:
-        raise serializers.ValidationError('Name cannot be more than 2 characters')
+class MovieSerializer(serializers.ModelSerializer):
 
-
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[name_len])
-    description = serializers.CharField()
-    activate = serializers.BooleanField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.activate = validated_data.get('activate', instance.activate)
-        instance.save()
-        return instance
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        # fields = ('id', 'name', 'description', 'activate')
+        # exclude = ('id')
 
     def validate(self, data):
 
@@ -31,9 +18,45 @@ class MovieSerializer(serializers.Serializer):
         else:
             return data
 
-    # def validate_name(self, value):
-    #
-    #     if len(value) < 3:
-    #         raise serializers.ValidationError('Name cannot be more than 3 characters')
-    #     else:
-    #         return value
+    def validate_name(self, value):
+
+        if len(value) < 3:
+            raise serializers.ValidationError('Name cannot be more than 3 characters')
+        else:
+            return value
+
+
+# def name_len(value):
+#     if len(value) < 2:
+#         raise serializers.ValidationError('Name cannot be more than 2 characters')
+#
+#
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[name_len])
+#     description = serializers.CharField()
+#     activate = serializers.BooleanField()
+#
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.activate = validated_data.get('activate', instance.activate)
+#         instance.save()
+#         return instance
+#
+#     def validate(self, data):
+#
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError("Name and Description should be different")
+#         else:
+#             return data
+#
+#     def validate_name(self, value):
+#
+#         if len(value) < 3:
+#             raise serializers.ValidationError('Name cannot be more than 3 characters')
+#         else:
+#             return value
