f02740ba011be96e975962104ffa0cdf0e6f5278
qioto <danylo3002@gmail.com>
2022-09-01 01:36:22 +0300

    Chapter 5: Views and Serializers     16. APIView Class

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import MovieDetailAV, MovieListAV
 
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>/', movie_details, name='movie-details'),
+    path('list/', MovieListAV.as_view(), name='movie-list'),
+    path('<int:pk>/', MovieDetailAV.as_view(), name='movie-details'),
 ]
