dd5906c90b9c4d5a25f4048b9ef028fc936a4e70
danyl <danylo3002@gmail.com>
2022-11-25 21:24:32 +0200

    Chapter 6: Postman and Project update     34. User Model     +     35. Temporary Login and Logout

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework import status, generics
 # from rest_framework import mixins
@@ -12,11 +13,20 @@ from .serializers import WatchListSerializer, StreamPlatformSerializer, ReviewSe
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        return Review.objects.all()
+
     def perform_create(self, serializer):
         pk = self.kwargs['pk']
         movie = WatchList.objects.get(pk=pk)
 
-        serializer.save(watchlist=movie)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=movie, review_user=review_user)
+
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie")
+
+        serializer.save(watchlist=movie, review_user=review_user)
 
 
 class ReviewList(generics.ListAPIView):
