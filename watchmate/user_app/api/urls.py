54a93eaa060d6e72f9b93a5533150d02c894a949
DanSheremeta <danylo3002@gmail.com>
2022-12-21 17:33:46 +0200

    Chapter 10: Token Authentication     45. Token Authentication - Part 5 (Registration)

--- watchmate/user_app/api/urls.py
+++ watchmate/user_app/api/urls.py
@@ -1,10 +1,10 @@
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import registrations_view
+from .views import registration_view
 from django.urls import path
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
-    path('sign-in/', registrations_view, name='sign-in'),
+    path('sign-in/', registration_view, name='sign-in'),
 
 ]
--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -1,14 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 from .serializers import RegistrationSerializer
+from .. import models
 
 
 @api_view(['POST'])
-def registrations_view(request):
+def registration_view(request):
 
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            user = serializer.save()
+            data['response'] = 'Registration successful'
+            data['username'] = user.username
+            data['email'] = user.email
+            data['token'] = Token.objects.get(user=user).key
+        else:
+            data = serializer.errors
+
+        return Response(data)
