dc6c6ba7ebed25f711edd58276ffe377810c6cc2
DanSheremeta <danylo3002@gmail.com>
2022-12-21 16:35:48 +0200

    Chapter 9: Token Authentication     44. Token Authentication - Part 4 (Registration)

--- watchmate/user_app/api/urls.py
+++ watchmate/user_app/api/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import registrations_view
 from django.urls import path
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
-    
+    path('sign-in/', registrations_view, name='sign-in'),
+
 ]
