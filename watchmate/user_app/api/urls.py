b5251f7456d581edf1ef85d5cf5da1dd8041cc24
DanSheremeta <danylo3002@gmail.com>
2022-12-21 18:21:34 +0200

    Chapter 10: Token Authentication     46. Token Authentication - Part 5 (Logout)

--- watchmate/user_app/api/urls.py
+++ watchmate/user_app/api/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import registration_view
+from .views import registration_view, logout_view
 from django.urls import path
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('sign-in/', registration_view, name='sign-in'),
+    path('logout/', logout_view, name='logout'),
 
 ]
