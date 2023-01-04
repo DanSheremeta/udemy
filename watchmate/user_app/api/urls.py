81dc3952555abafc7ed43cb63f0a118f6fe5a33f
DanSheremeta <danylo3002@gmail.com>
2023-01-04 18:45:45 +0200

    Chapter 12: JWT Authentication     49. JWT Authentication - Access Token and Refresh Token     +     50. JWT Authentication - Login

--- watchmate/user_app/api/urls.py
+++ watchmate/user_app/api/urls.py
@@ -1,11 +1,17 @@
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView, TokenVerifyView
+
 from .views import registration_view, logout_view
 from django.urls import path
 
 
 urlpatterns = [
-    path('login/', obtain_auth_token, name='login'),
-    path('sign-in/', registration_view, name='sign-in'),
-    path('logout/', logout_view, name='logout'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    # path('login/', obtain_auth_token, name='login'),
+    # path('sign-in/', registration_view, name='sign-in'),
+    # path('logout/', logout_view, name='logout'),
 
 ]
--- watchmate/watchmate/settings.py
+++ watchmate/watchmate/settings.py
@@ -113,13 +113,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        # 'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
     ],
     # 'DEFAULT_PERMISSION_CLASSES': [
     #      'rest_framework.permissions.IsAuthenticated',
-    # ]
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework.authentication.BasicAuthentication',
     # ]
 }
 
