55046529338d8a0f66cdf363b67c6e48a40d58e3
DanSheremeta <danylo3002@gmail.com>
2023-01-10 23:00:45 +0200

    Chapter 16: Automated API Testing     62. API Testing - Registration

--- watchmate/user_app/api/serializers.py
+++ watchmate/user_app/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.core.validators import validate_email
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
--- watchmate/user_app/tests.py
+++ watchmate/user_app/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+
+class RegisterTestCase(APITestCase):
+
+    def test_register(self):
+        data = {
+            'username': 'testname',
+            'email': 'testemail@gmail.com',
+            'password': 'testpassword',
+            'password2': 'testpassword',
+        }
+        response = self.client.post(reverse('sign-in'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_register_incorrect_password(self):
+        data = {
+            'username': 'testname',
+            'email': 'testemail@gmail.com',
+            'password': 'testpassword',
+            'password2': 'testpassword2',
+        }
+        response = self.client.post(reverse('sign-in'), data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
