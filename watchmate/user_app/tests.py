00d031f0c34b44958c3be09b3cfe1653975523c6
DanSheremeta <danylo3002@gmail.com>
2023-01-13 20:03:18 +0200

    Chapter 16: Automated API Testing     63. API Testing - Login and Logout

--- watchmate/user_app/tests.py
+++ watchmate/user_app/tests.py
@@ -8,22 +8,45 @@ from rest_framework.authtoken.models import Token
 
 class RegisterTestCase(APITestCase):
 
-    def test_register(self):
-        data = {
+    def setUp(self):
+        self.data = {
             'username': 'testname',
             'email': 'testemail@gmail.com',
             'password': 'testpassword',
             'password2': 'testpassword',
         }
-        response = self.client.post(reverse('sign-in'), data)
+
+    def test_register(self):
+        response = self.client.post(reverse('sign-in'), self.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_register_incorrect_password(self):
-        data = {
-            'username': 'testname',
-            'email': 'testemail@gmail.com',
-            'password': 'testpassword',
-            'password2': 'testpassword2',
-        }
-        response = self.client.post(reverse('sign-in'), data)
+        self.data['password2'] = 'wrong'
+        response = self.client.post(reverse('sign-in'), self.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self):
+        self.data = {
+            'username': 'test',
+            'password': 'test0password'
+        }
+        self.user = User.objects.create_user(username='test',
+                                             password='test0password')
+
+    def test_login(self):
+        response = self.client.post(reverse('login'), self.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_incorrect_login(self):
+        self.data['password'] = 'wrong'
+        response = self.client.post(reverse('login'), self.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_logout(self):
+        self.token = Token.objects.get(user__username=self.data['username'])
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
